77a2e04b9ca1571d2f908025d3e97e545c6ede63
nillem-14 <71769239+nillem-14@users.noreply.github.com>
2023-02-08 16:24:16 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args[1] == "h" || args[1] == "help" {
-        println!("<filePath> [-s <searched word or regex>] [-i/--insensitive] [--from <line usize>] [--to <line usize>]  \n Help : help (h) \n Version: version (v)");
+        println!("<filePath> [-s <searched word or regex>] [-i/-insensitive] [-from <line usize>] [-to <line usize>]  \n Help : help (h) \n Version: version (v)");
         process::exit(1);
     } else if args[1] == "v" || args[1] == "version" {
         println!("V.0.0.1");
