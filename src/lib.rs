bbba0b34796b45f645b49cb56de057c3e8472e0b
nillem-14 <71769239+nillem-14@users.noreply.github.com>
2023-02-08 12:08:09 +0100

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -4,69 +4,31 @@ use std::{fs, usize};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    let mut results: Vec<&str> = Vec::new();
-    let mut numl: Vec<usize> = Vec::new();
 
     match config.query {
         Some(query) => {
-            if Regex::new(&query).is_err() {
-                results = if config.case_sensitive.unwrap_or(true) {
-                    search(&query, &contents, config.from.as_ref(), config.to.as_ref()).0
-                } else {
-                    search_case_insensitive(
-                        &query,
-                        &contents,
-                        config.from.as_ref(),
-                        config.to.as_ref(),
-                    )
-                    .0
-                };
-                numl = if config.case_sensitive.unwrap_or(true) {
-                    search(&query, &contents, config.from.as_ref(), config.to.as_ref()).1
-                } else {
-                    search_case_insensitive(
-                        &query,
-                        &contents,
-                        config.from.as_ref(),
-                        config.to.as_ref(),
-                    )
-                    .1
-                };
+            let (results, numl) = search(
+                &query,
+                &contents,
+                config.from.as_ref(),
+                config.to.as_ref(),
+                config.before,
+                config.after,
+                config.case_sensitive,
+            );
+            if results.is_empty() {
+                println!("No results found");
             } else {
-                results = if config.case_sensitive.unwrap_or(true) {
-                    search_re(&query, &contents, config.from.as_ref(), config.to.as_ref()).0
-                } else {
-                    search_case_insensitive_re(
-                        &query,
-                        &contents,
-                        config.from.as_ref(),
-                        config.to.as_ref(),
-                    )
-                    .0
-                };
-
-                numl = if config.case_sensitive.unwrap_or(true) {
-                    search_re(&query, &contents, config.from.as_ref(), config.to.as_ref()).1
-                } else {
-                    search_case_insensitive_re(
-                        &query,
-                        &contents,
-                        config.from.as_ref(),
-                        config.to.as_ref(),
-                    )
-                    .1
-                };
-            }
-            for (num, _line) in results.iter().enumerate() {
-                println!("{}. {}", numl[num], results[num]);
+                for (num, line) in results.iter().enumerate() {
+                    println!("{}: {}", numl[num], line);
+                }
             }
         }
         None => {
-            numl = run_shows(&contents, config.from, config.to).1;
-            results = run_shows(&contents, config.from, config.to).0;
+            let (results, numl) = run_shows(&contents, config.from, config.to);
 
             for (num, _line) in results.iter().enumerate() {
-                println!("{}, {}", numl[num], results[num]);
+                println!("{}: {}", numl[num], results[num]);
             }
         }
     }
@@ -76,9 +38,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub filename: String,
     pub query: Option<String>,
-    pub case_sensitive: Option<bool>,
+    pub case_sensitive: bool,
     pub from: Option<usize>,
     pub to: Option<usize>,
+    pub before: usize,
+    pub after: usize,
 }
 
 impl Config {
@@ -88,35 +52,51 @@ impl Config {
         }
         let filename = args[1].clone();
 
-        let mut case_sensitive = None;
+        let mut case_sensitive = false;
         let mut query = None;
         let mut from = None;
         let mut to = None;
+        let mut after = 0;
+        let mut before = 0;
 
         let mut i = 2;
 
         while i < args.len() {
             match args[i].as_ref() {
-                "-s" | "--search" => {
+                "-s" | "-search" => {
                     if i + 1 >= args.len() {
                         return Err("Not enough arguments \n h for help");
                     }
                     query = Some(args[i + 1].clone());
                     i += 1;
                 }
-                "-i" | "--insensitive" => case_sensitive = Some(false),
-                "--from" => {
+                "-i" | "-insensitive" => case_sensitive = true,
+                "-from" => {
                     if i + 1 >= args.len() {
                         return Err("not enough arguments \n h for help");
                     }
                     from = Some(args[i + 1].parse().unwrap());
                     i += 1;
                 }
-                "--to" => {
+                "-to" => {
                     if i + 1 >= args.len() {
                         return Err("not enough arguments \n h for help");
                     }
                     to = Some(args[i + 1].parse().unwrap());
+                    i += 1;
+                }
+                "-a" => {
+                    if i + 1 >= args.len() {
+                        return Err("not enough arguments \n h for help");
+                    }
+                    after = args[i + 1].parse().unwrap();
+                    i += 1;
+                }
+                "-b" => {
+                    if i + 1 >= args.len() {
+                        return Err("not enough arguments \n h for help");
+                    }
+                    before = args[i + 1].parse().unwrap();
                     i += 1;
                 }
                 _ => return Err("invalid argument format \n h for help"),
@@ -129,6 +109,8 @@ impl Config {
             case_sensitive,
             from,
             to,
+            before,
+            after,
         })
     }
 }
@@ -142,7 +124,8 @@ pub fn run_shows(
     let mut num_lignes: Vec<usize> = Vec::new();
 
     for (i, line) in contents.lines().enumerate() {
-        if from.map_or(true, |f| i >= f) && to.map_or(true, |t| i <= t) {
+        if from.map_or(true, |f| i >= f) && to.map_or(true, |t| i <= t && !results.contains(&line))
+        {
             num_lignes.push(i);
             results.push(line);
         }
@@ -155,78 +138,34 @@ pub fn search<'a>(
     contents: &'a str,
     from: Option<&usize>,
     to: Option<&usize>,
+    before: usize,
+    after: usize,
+    case_sensitive: bool,
 ) -> (Vec<&'a str>, Vec<usize>) {
     let mut results = Vec::new();
     let mut num_lignes: Vec<usize> = Vec::new();
-
-    for (i, line) in contents.lines().enumerate() {
-        if from.map_or(true, |f| i >= *f) && to.map_or(true, |t| i <= *t) && line.contains(query) {
-            num_lignes.push(i);
-            results.push(line);
-        }
-    }
-    (results, num_lignes)
-}
-
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str,
-    from: Option<&usize>,
-    to: Option<&usize>,
-) -> (Vec<&'a str>, Vec<usize>) {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    let mut num_lignes: Vec<usize> = Vec::new();
-
-    for (i, line) in contents.lines().enumerate() {
-        if from.map_or(true, |f| i >= *f)
-            && to.map_or(true, |t| i <= *t)
-            && line.to_lowercase().contains(&query)
-        {
-            num_lignes.push(i);
-            results.push(line);
-        }
-    }
-    (results, num_lignes)
-}
-
-pub fn search_re<'a>(
-    query: &str,
-    contents: &'a str,
-    from: Option<&usize>,
-    to: Option<&usize>,
-) -> (Vec<&'a str>, Vec<usize>) {
-    let mut results = Vec::new();
-    let mut num_lignes: Vec<usize> = Vec::new();
-    let re = Regex::new(query).unwrap();
+    let re = if case_sensitive {
+        Regex::new(&format!("(?i){}", query)).unwrap()
+    } else {
+        Regex::new(query).unwrap()
+    };
 
     for (i, line) in contents.lines().enumerate() {
         if from.map_or(true, |f| i >= *f) && to.map_or(true, |t| i <= *t) && re.is_match(line) {
-            num_lignes.push(i);
-            results.push(line);
-        }
-    }
-    (results, num_lignes)
-}
-
-pub fn search_case_insensitive_re<'a>(
-    query: &str,
-    contents: &'a str,
-    from: Option<&usize>,
-    to: Option<&usize>,
-) -> (Vec<&'a str>, Vec<usize>) {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    let mut num_lignes: Vec<usize> = Vec::new();
-    let re = Regex::new(&query).unwrap();
-
-    for (i, line) in contents.lines().enumerate() {
-        if from.map_or(true, |f| i >= *f)
-            && to.map_or(true, |t| i <= *t)
-            && re.is_match(&line.to_lowercase())
-        {
-            num_lignes.push(i);
-            results.push(line);
+            let mut edge: usize = 0;
+            let mut edge_final: usize = contents.lines().count();
+            if before < i {
+                edge = i - before;
+            }
+            if i + after < contents.lines().count() {
+                edge_final = i + after;
+            }
+            for j in edge..edge_final + 1 {
+                if !num_lignes.contains(&j) {
+                    num_lignes.push(j);
+                    results.push(contents.lines().nth(j).unwrap());
+                }
+            }
         }
     }
     (results, num_lignes)
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args[1] == "h" || args[1] == "help" {
-        println!("<filePath> [-s <searched word>] [-i/insensistive] [--from <usize>] [--to <usize>]  \n Help : help (h) \n Version: version (v)");
+        println!("<filePath> [-s <searched word or regex>] [-i/--insensitive] [--from <line usize>] [--to <line usize>]  \n Help : help (h) \n Version: version (v)");
         process::exit(1);
     } else if args[1] == "v" || args[1] == "version" {
         println!("V.0.0.1");
         process::exit(1);
     } else {
         let config = Config::new(&args).unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {} \n -h for help", err);
+            println!("Problem parsing arguments: {}", err);
             process::exit(1);
         });
         if let Err(e) = minigrep::run(config) {
